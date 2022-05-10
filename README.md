34576389455189b4644dca4098a5ce1efeeb04ec
a-peltier <26309854+a-peltier@users.noreply.github.com>
2022-05-10 16:55:04 +0200

    version 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # stitches-simple-css-reset
-Josh Comeau's CSS Reset for Stitches
+
+Josh Comeaus's [Reset CSS](https://www.joshwcomeau.com/css/custom-css-reset/) for [stitches](https://github.com/modulz/stitches).
+
+## Installation:
+
+`yarn add stitches-reset`
+
+or
+
+`npm install stitches-reset`
+or
+
+`pnpm add stitches-reset`
+
+## Credits
+
+All credit goes to Josh Comeau the CSS implementation and hauptrolle for the [stithces-reset](https://github.com/hauptrolle/stitches-reset) example.
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+export const reset = {
+  "*, *::before, *::after": {
+    boxSizing: "border-box",
+  },
+  "*": {
+    margin: 0,
+    padding: 0,
+  },
+  "html, body": {
+    height: "100%",
+  },
+  body: {
+    lineHeight: 1.5,
+    "-webkit-font-smoothing": "antialiased",
+  },
+  "img, picture, video, canvas, svg": {
+    display: "block",
+    maxWidth: "100%",
+  },
+  "input, button, textarea, select": {
+    font: "inherit",
+  },
+  "p, h1, h2, h3, h4, h5, h6": {
+    overflowWrap: "break-word",
+  },
+};
